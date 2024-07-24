197bb54fc2aaccb611529cde49b3a2d3ca896e39
Yuki-42 <kylepannan@gmail.com>
2024-07-24 12:53:41 +1000

    Added images table

--- database/schema.sql
+++ database/schema.sql
@@ -1,9 +1,19 @@
-CREATE TABLE contact_requests (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE contact_requests
+(
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    phone TEXT,
-    subject TEXT NOT NULL,
-    message TEXT NOT NULL
+    name       TEXT NOT NULL,
+    email      TEXT NOT NULL,
+    phone      TEXT,
+    subject    TEXT NOT NULL,
+    message    TEXT NOT NULL
+);
+
+CREATE TABLE images
+(
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    name        TEXT NOT NULL,
+    path        TEXT NOT NULL,
+    description TEXT NOT NULL
 );
